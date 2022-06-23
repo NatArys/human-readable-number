6872dea72d2c0e3e527cf9e0542f2998a8c06b26
NatArys <pok13nat@yahoo.com>
2022-06-23 15:36:13 +0700

    feat: change function

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
 module.exports = function toReadable(number) {
+  let numName0 = '',
+    numName1 = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'],
+    numName10 = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'],
+    numdig = [];
 
-  let numName;
-
-  numName[1] = ['', 'one ', 'two ', 'three ', 'four ', 'five ', 'six ', 'seven ', 'eight ', 'nine ', 'ten ', 'eleven ', 'twelve ', 'thirteen ', 'fourteen ', 'fifteen ', 'sixteen ', 'seventeen ', 'eighteen ', 'nineteen ']
-  numName[10] = ['', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-
-  let numDigit;
-
-  // numDigit[0] = number % 10;
-  // numDigit[10] = number % 100 - numDigit[0];
-  // numDigit[100] = number % 1000 - numDigit[10] - numDigit[0];
-
+  numdig[0] = number % 10;
+  numdig[1] = Math.floor(number / 10) % 10;
+  numdig[2] = Math.floor(number / 100) % 10;
   
-numDigit = String(number).split('')
+  if (number == 0) return 'zero';
+  numdig[2] > 0 ? numName0 += numName1[numdig[2]] + ' hundred ' : '';
+  numdig[1] > 1 ? numName0 += numName10[numdig[1] - 2] + ' ' : numdig[0] = numdig[1]*10 + numdig[0];
+  numName0 += numName1[numdig[0]];
 
-console.log(numDigit[0])
-
-
+  return numName0.trim();
 }
-toReadable(1234)
